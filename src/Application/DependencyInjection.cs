50cf460ddb0e716ee1db5bb3c25bb19d682ad657
Hashim Elidrissi <harakill@yahoo.com>
2022-02-24 15:41:27 +0200

    complete code

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,7 +1,24 @@
-﻿namespace Application
-{
-    public class DependencyInjection
-    {
+﻿using System.Reflection;
+using Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+
+            return services;
+        }
     }
 }
