6723e10a71884d76ed45a6d6c5cb52705b6a4c88
Hashim Elidrissi <harakill@yahoo.com>
2022-02-24 18:17:39 +0200

    add IApplicationDbContext.cs

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Panic> Panics { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
