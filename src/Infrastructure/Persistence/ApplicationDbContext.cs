61c72e588947251b5ac85553c296fbae106c94bb
Hashim Elidrissi <harakill@yahoo.com>
2022-02-25 07:50:17 +0200

    configer database

---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Interfaces;
+using Domain.Common;
+using Domain.Entities;
+using Duende.IdentityServer.EntityFramework.Options;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Persistence
+{
+    internal class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDateTime _dateTime;
+
+        public ApplicationDbContext(
+            DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions,
+            ICurrentUserService currentUserService) 
+            : base(options, operationalStoreOptions)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public DbSet<Panic> Panics => Set<Panic>();
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = _currentUserService.UserId;
+                        entry.Entity.Created = _dateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = _currentUserService.UserId;
+                        entry.Entity.LastModified = _dateTime.Now;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
