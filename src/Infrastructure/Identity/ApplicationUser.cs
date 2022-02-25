03e96e6f9de094aebd0b07200e3b3817e7174981
Hashim Elidrissi <harakill@yahoo.com>
2022-02-25 07:00:08 +0200

    complete Identity Service

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity
 {
-    internal class ApplicationUser
+    internal class ApplicationUser : IdentityUser
     {
     }
 }
