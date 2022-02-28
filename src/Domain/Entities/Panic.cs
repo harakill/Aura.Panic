15eaa9e35665958a3bfed9b51aee0555bd652652
Hashim Elidrissi <harakill@yahoo.com>
2022-02-28 15:17:34 +0200

    add ? to string properties to get rid of null warnings

--- src/Domain/Entities/Panic.cs
+++ src/Domain/Entities/Panic.cs
@@ -4,10 +4,10 @@ namespace Domain.Entities
     public class Panic : AuditableEntity
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Phone { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Note { get; set; }
+        public string? Note { get; set; }
     }
 }
