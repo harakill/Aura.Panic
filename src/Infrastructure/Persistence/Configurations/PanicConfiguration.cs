d3aa30406ebaf16de5d41c28833373769c0b2294
Hashim Elidrissi <harakill@yahoo.com>
2022-02-28 15:19:04 +0200

    add PanicConfiguration.cs

---
+++ src/Infrastructure/Persistence/Configurations/PanicConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    internal class PanicConfiguration : IEntityTypeConfiguration<Panic>
+    {
+        public void Configure(EntityTypeBuilder<Panic> builder)
+        {
+            builder.Property(fn => fn.FullName)
+                .IsRequired()
+                .HasMaxLength(128);
+
+            builder.Property(ph => ph.Phone)
+                .IsRequired()
+                .HasMaxLength(16);
+
+            builder.Property(n => n.Note)
+                .HasMaxLength(512);
+        }
+    }
+}
