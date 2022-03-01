2e0c82ce3cc925156180ffd162568353d6645a7b
Hashim Elidrissi <harakill@yahoo.com>
2022-03-01 09:14:35 +0200

    add DeletePanicCommand.cs

---
+++ src/Application/Panics/Commands/DeletePanicCommand.cs
@@ -0,0 +1,41 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Panics.Commands
+{
+    public class DeletePanicCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePanicCommandHandler : IRequestHandler<DeletePanicCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeletePanicCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeletePanicCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Panics.FindAsync(request.Id, cancellationToken);
+
+            if(entity == null)
+                throw new NotFoundException(nameof(Panic), request.Id);
+
+            _context.Panics.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
