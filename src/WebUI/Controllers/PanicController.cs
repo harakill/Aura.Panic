4045a74085e83650b718b6939db5194c10b78602
Hashim Elidrissi <harakill@yahoo.com>
2022-03-01 13:06:32 +0200

    add PanicController.cs

---
+++ src/WebUI/Controllers/PanicController.cs
@@ -0,0 +1,16 @@
+﻿using Application.Panics.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    //[Authorize]
+    public class PanicController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<PanicListDto>> GetAllPanics(GetPanicsQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
