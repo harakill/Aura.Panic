c51fe2d9786cff1ea52985fdbb3a125dd04cf4f0
Hashim Elidrissi <harakill@yahoo.com>
2022-03-01 13:05:32 +0200

    add Queries class with DTO

---
+++ src/Application/Panics/Queries/GetPanicsQuery.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Panics.Queries
+{
+    public class GetPanicsQuery : IRequest<PanicListDto>
+    {
+    }
+
+    public class GetPanicsQueryHandler : IRequestHandler<GetPanicsQuery, PanicListDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetPanicsQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PanicListDto> Handle(GetPanicsQuery request, CancellationToken cancellationToken)
+        {
+            return (PanicListDto)(await _context.Panics.ToListAsync())
+                .OrderBy(fn => fn.FullName);
+        }
+    }
+}
---
+++ src/Application/Panics/Queries/PanicListDto.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Panics.Queries
+{
+    public class PanicListDto : IMapFrom<Panic>
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Phone { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? Note { get; set; }
+    }
+}
