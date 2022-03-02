2426f70eb1df49f136e9f735aeadfa389fea2252
Hashim Elidrissi <harakill@yahoo.com>
2022-03-02 12:00:39 +0200

    complete panic api (add, update and delete).

--- src/WebUI/Controllers/PanicController.cs
+++ src/WebUI/Controllers/PanicController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Panics.Queries;
+﻿using Application.Panics.Commands;
+using Application.Panics.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,36 @@ namespace WebUI.Controllers
     public class PanicController : ApiControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<PanicListDto>> GetAllPanics(GetPanicsQuery query)
+        public async Task<ActionResult<PanicDto>> Get()
         {
-            return await Mediator.Send(query);
+            return await Mediator.Send(new GetPanicsQuery());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreatePanicCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, UpdatePanicCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeletePanicCommand { Id = id });
+
+            return NoContent();
         }
     }
 }
