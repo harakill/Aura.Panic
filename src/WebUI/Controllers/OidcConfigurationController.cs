e1a75bf1d228c7390c587871d4a0a7832013a07a
Hashim Elidrissi <harakill@yahoo.com>
2022-03-02 14:12:47 +0200

    add configuration for front end service.

---
+++ src/WebUI/Controllers/OidcConfigurationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class OidcConfigurationController : Controller
+    {
+        private readonly ILogger<OidcConfigurationController> logger;
+
+        public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
+
+        public OidcConfigurationController(
+            IClientRequestParametersProvider clientRequestParametersProvider, 
+            ILogger<OidcConfigurationController> _logger)
+        {
+            ClientRequestParametersProvider = clientRequestParametersProvider;
+            logger = _logger;
+        }
+
+        [HttpGet("_configuration/{clientId}")]
+        public IActionResult GetClientRequestParameters([FromRoute] string clientId)
+        {
+            var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+            return Ok(parameters);
+        }
+    }
+}
